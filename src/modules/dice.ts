c877bee25be6f2ba141c0ec070c9fc1cee3223ce
Rick James <mail.rickyjames@gmail.com>
2020-06-23 12:36:33 +1000

    Draw Custom Die function to draw a die anywhere, anywhen

--- src/modules/dice.ts
+++ src/modules/dice.ts
@@ -1,6 +1,6 @@
 import { Vec2d } from '../types'
 import { GRID } from '../const'
-import { plethoric, pallor } from '../colors'
+import { plethoric, pallor, grey } from '../colors'
 import { GameObjects, Scene } from 'phaser'
 import { colorForPlayer, Player } from './turn'
 
@@ -18,6 +18,32 @@ export function redrawDice(n: number, p: Vec2d, gfx: GameObjects.Graphics, playe
 
   if (n != 0) {
     gfx.fillStyle(colorForPlayer(player))
+    gfx.fillRoundedRect(0, 0, GRID - RADIUS * 2, GRID - RADIUS * 2, RADIUS)
+  }
+  gfx.fillStyle(pallor)
+  if (n == 1 || n == 3 || n == 5) {
+    gfx.fillCircle(36, 36, RADIUS)
+  }
+  if (n == 2 || n == 3 || n == 4 || n == 5 || n == 6) {
+    gfx.fillCircle(16, 16, RADIUS)
+    gfx.fillCircle(56, 56, RADIUS)
+  }
+  if (n == 4 || n == 5 || n == 6) {
+    gfx.fillCircle(16, 56, RADIUS)
+    gfx.fillCircle(56, 16, RADIUS)
+  }
+  if (n == 6) {
+    gfx.fillCircle(56, 36, RADIUS)
+    gfx.fillCircle(16, 36, RADIUS)
+  }
+}
+
+export function drawCustomDie(n: number, x:number, y:number, gfx: GameObjects.Graphics, c: 8553090 = grey) {
+  gfx.setPosition(x, y)
+  gfx.clear()
+
+  if (n != 0) {
+    gfx.fillStyle(c)
     gfx.fillRoundedRect(0, 0, GRID - RADIUS * 2, GRID - RADIUS * 2, RADIUS)
   }
   gfx.fillStyle(pallor)
